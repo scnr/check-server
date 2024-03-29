e805b7e6344f16c91b11c5f0eace250304befa4a
Tasos Laskos <tasos.laskos@gmail.com>
2024-03-29 07:08:12 +0200

    Added ping

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org/'
 
 gem 'puma'
+gem 'json'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,10 +1,37 @@
 require 'pp'
+require 'json'
 require 'sinatra/base'
 
 class CheckServer < Sinatra::Base
 
+  @@sesssions = {}
+
+  get '/:id' do
+    JSON.pretty_generate @@sesssions[params[:id]]
+  end
+
   get '/:id/rfi' do
     '705cd559b16e6946826207c2199bd890'
+  end
+
+  get '/:id/ping' do
+    @@sesssions[params[:id]] = request.env
+  end
+
+  post '/:id/ping' do
+    @@sesssions[params[:id]] = request.env
+  end
+
+  put '/:id/ping' do
+    @@sesssions[params[:id]] = request.env
+  end
+
+  delete '/:id/ping' do
+    @@sesssions[params[:id]] = request.env
+  end
+
+  delete '/:id' do
+    @@sesssions.delete params[:id]
   end
 
 end
